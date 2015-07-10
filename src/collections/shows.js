97ee680a0108f65840406ece43520624c48d9ea5
Jeff Auriemma <Jeff_Auriemma@discovery.com>
2015-07-10 00:35:46 -0500

    add date collection

---
+++ src/collections/shows.js
@@ -0,0 +1,34 @@
+var Backbone = require('backbone');
+
+module.exports = Backbone.Collection.extend({
+    "index": 0,
+    "url": "http://jdauriemma.startlogic.com/dsb/get-events.php",
+    "initialize": function () {
+        'use strict';
+        this.listenTo(this, 'sync', function () {
+            console.log(this);
+        });
+    },
+    "comparator": function (model) {
+        'use strict';
+        return +model.get('time');
+    },
+    "parse": function (response) {
+        'use strict';
+        this.index = response.index;
+        return response.data;
+    },
+    "getIncrement": function (back) {
+        'use strict';
+        var increment = back ? this.index - 20 : this.length + this.index;
+        if (increment < 0) {
+            increment = 0;
+        }
+        this.fetch({
+            "remove": false,
+            "data": {
+                "index": increment
+            }
+        });
+    }
+});
