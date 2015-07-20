fb6a65835d4ce5e09458b2c85096fe591c8d6845
Jeff Auriemma <Jeff_Auriemma@discovery.com>
2015-07-19 22:43:30 -0500

    add soundcloud playing functionality and visual sugar

--- src/views/about.js
+++ src/views/about.js
@@ -14,6 +14,6 @@ module.exports = ChildView.extend({
                     "carousel": true
                 }
             );
-        }, 500);
+        }, 1000);
     }
 });
--- src/views/audio.js
+++ src/views/audio.js
@@ -1,7 +1,59 @@
-var Mn = require('backbone.marionette'),
+/* global SC */
+var _ = require('underscore'),
+    Mn = require('backbone.marionette'),
     template = require('../templates/audio.hbs');
 
 module.exports = Mn.ItemView.extend({
     "template": template,
-    "className": "track"
+    "className": "track",
+    "events": {
+        "click .cover": "togglePlay"
+    },
+    "togglePlay": function (firstPlay) {
+        'use strict';
+        var id,
+            url;
+        if (!this.sound) {
+            this.state('loading');
+            url = this.$('.art').data('stream');
+            id = url.match(/\/tracks\/\w.+?\//)[0];
+
+            SC.stream(id, {
+                "useHTML5Audio": true,
+                "preferFlash": false
+            }, _.bind(function (sound) {
+                this.sound = sound;
+                this.playOrPause();
+                this.sound.onfinish = _(this.state).bind(this, 'paused');
+            }, this));
+        } else {
+            this.playOrPause();
+        }
+    },
+    "playOrPause": function () {
+        'use strict';
+        var state = this.sound.getState();
+        if (state === 'playing') {
+            this.sound.pause();
+            this.state('paused');
+        } else {
+            this.sound.play();
+            this.state('playing');
+        }
+    },
+    "state": function (state) {
+        'use strict';
+        var $el = this.$('.play');
+            $el.removeClass('fa-play fa-spinner fa-spin fa-pause');
+        switch (state) {
+            case 'loading':
+                $el.addClass('fa-spinner fa-spin');
+                break;
+            case 'paused':
+                $el.addClass('fa-play');
+                break;
+            case 'playing':
+                $el.addClass('fa-pause');
+        }
+    }
 });
